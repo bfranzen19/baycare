60cd9aa7a44df159ecb0ec15ca7532d791af41fd
bfranzen19 <btfranzen@gmail.com>
2018-03-05 15:29:37 -0700

    about us page scraper operational

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php
 // URL to scrape
-  $url = "";
+  $url = "https://baycare.org/about-us";
 
 // SCRAPER
   $curl = curl_init($url);
@@ -16,7 +16,7 @@
 // BREAK SCRIPTS
 // strips all tags EXCEPT for tags listed to the right (second input). if it's removed from
 // the second input, it's going to be stripped.
-  $data = strip_tags($data, "<a><strong><script><b><p><br><hr><h1><h2><h3><div><ul><li><section><article>");
+  $data = strip_tags($data, "<style><span><a><strong><b><p><br><hr><h1><h2><h3><h4><h5><h6><div><ul><li>");
 
 // ECHO DATA TO JS SCRIPT IN HTML FILE
   echo "loadData(".json_encode($data).")";
