aa92942a0dbe18017175cc20126e2b3c9f85eba0
bfranzen19 <btfranzen@gmail.com>
2018-03-06 11:44:20 -0700

    maternityServices and duringPregnancy scraping and inital styling complete.

--- maternityServices.php
+++ maternityServices.php
@@ -1,6 +1,6 @@
 <?php
 // URL to scrape
-  $url = "https://baycare.org/about-us";
+  $url = "https://baycare.org/services/maternity?hcmacid=a0Z1a000001l0sZEAQ";
 
 // SCRAPER
   $curl = curl_init($url);
@@ -12,13 +12,11 @@
 
 // CHANGE PROBLEMATIC STRINGS
   $data = str_replace('href="/', 'href="https://baycare.org/', $data);
-  $data = str_replace('<h2', '<h3', $data);
-  $data = str_replace('</h2>', '</h3>', $data);
 
 // BREAK SCRIPTS
 // strips all tags EXCEPT for tags listed to the right (second input). if it's removed from
 // the second input, it's going to be stripped.
-  $data = strip_tags($data, "<style><span><a><p><br><hr><h1><h2><h3><h4><h5><h6><div><ul><li>");
+  $data = strip_tags($data, "<span><a><p><br><hr><h1><h2><h3><h4><h5><h6><div><ul><li>");
 
 // ECHO DATA TO JS SCRIPT IN HTML FILE
   echo "loadData(".json_encode($data).")";
